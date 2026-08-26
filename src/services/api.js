7d14f53b93ea2292cbe9eedd9ce610d7823529db
oscar-LCP <ofcruz@uniboyaca.edu.co>
2026-08-26 15:44:23 -0500

    Lógica de todo el taller

---
+++ src/services/api.js
@@ -0,0 +1,25 @@
+import Pokemon from "..models/Pokemon.js";
+
+const API_URL = "https://pokeapi.co/api/v2/pokemon/";
+
+export async function fetchPokemon(id) {
+    try {
+        const res = await fetch(API_URL + id);
+        if (!res.ok) throw new Error("No se encontró el Pokémon");
+        const data = await res.json();
+
+        // Extraer los tipos del Pokémon
+        const types = data.types.map(t => t.type.name);
+
+        // Crear una instancia de la clase Pokemon
+        return new Pokemon(
+            data.id,
+            data.name,
+            types,
+            data.sprites.other["official-artwork"].front_default
+        );
+    }   catch (error) {
+        console.error(error);
+        return null;
+    }
+}
