9bb28a3d757356f1c9e23bd020fe727801b7e068
oscar-LCP <ofcruz@uniboyaca.edu.co>
2026-08-26 20:58:12 -0500

    Conexión a la API de Pokemon, para poder extraer los distintos atributos del pokemon

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-import Pokemon from "..models/Pokemon.js";
+import Pokemon from "../models/Pokemon.js";
 
 const API_URL = "https://pokeapi.co/api/v2/pokemon/";
 
@@ -11,15 +11,33 @@ export async function fetchPokemon(id) {
         // Extraer los tipos del Pokémon
         const types = data.types.map(t => t.type.name);
 
+        // Extraer las habilidades
+        const abilities = data.abilities.map(a => a.ability.name);
+
+        // Extraer las estadísticas
+        const stats = data.stats.map(s => ({
+            stat: s.stat.name,
+            base: s.base_stat
+        }));
+
         // Crear una instancia de la clase Pokemon
         return new Pokemon(
             data.id,
             data.name,
             types,
-            data.sprites.other["official-artwork"].front_default
+            data.sprites.other["official-artwork"].front_default,
+            data.height,
+            data.weight,
+            abilities,
+            stats
         );
+
     }   catch (error) {
         console.error(error);
         return null;
     }
 }
+
+
+
+
