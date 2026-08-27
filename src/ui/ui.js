ea72dcf6e8f1d61f584ec0a1c2b1b0b27c88288c
oscar-LCP <ofcruz@uniboyaca.edu.co>
2026-08-26 20:50:04 -0500

    Configuración de UI y sus acciones, así como funciones

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -3,9 +3,10 @@ export function showPokemon(pokemon) {
 
     // Datos del Pokemón
     document.getElementById("pokemon-img").src = pokemon.sprite;
+    document.getElementById("pokemon-img").alt = `Imagen de ${pokemon.name}`;
     document.getElementById("pokemon-name").textContent = capitalize(pokemon.name);
-    document.getElementById("pokemon-type").textContent = "#" +pokemon.id.toString().padStart(3, "0");
-
+    document.getElementById("pokemon-id").textContent = "#" +pokemon.id.toString().padStart(3, "0");
+    
     // Tipos
     const typesDiv = document.querySelector(".types");
     typesDiv.innerHTML = "";
@@ -15,8 +16,50 @@ export function showPokemon(pokemon) {
         span.textContent = capitalize(t);
         typesDiv.appendChild(span);
     });
+
+    // Mostrar modal
+    document.getElementById("pokemon-img").onclick = () => showModal(pokemon);
 }
 
 function capitalize(word) {
     return word.charAt(0).toUpperCase() + word.slice(1);
 }
+
+function showModal(pokemon) {
+    const modal = document.getElementById("pokemon-modal");
+
+    //Estadísticas básicas del pokemon
+    document.getElementById("modal-img").src = pokemon.sprite;
+    document.getElementById("modal-img").alt = `Imagen de ${pokemon.name}`;
+    document.getElementById("modal-name").textContent = capitalize(pokemon.name);
+    document.getElementById("modal-id").textContent = "#" + pokemon.id.toString().padStart(3, "0");
+
+    document.getElementById("modal-height").textContent = pokemon.size / 10 + "m";
+    document.getElementById("modal-weight").textContent = pokemon.weight / 10 + "kg";
+
+    // Habilidades del pokemon
+    const abilitiesList = document.getElementById("modal-abilities");
+    abilitiesList.innerHTML = "";
+
+    pokemon.abilities.forEach(ability => {
+        const li = document.createElement("li");
+        li.textContent = capitalize(ability);
+        abilitiesList.appendChild(li);
+    });
+
+    // estadísticas del pokemon
+    const statsList = document.getElementById("modal-stats");
+    statsList.innerHTML = "";
+    
+    pokemon.stats.forEach(stat => {
+        const li = document.createElement("li");
+        li.textContent = `${capitalize(stat.stat)}: ${stat.base}`;
+        statsList.appendChild(li);
+    });
+
+    //modal
+    modal.classList.remove("hidden");
+}
+
+// Cerrar modal
+document.querySelector(".close-modal").addEventListener("click", () => {document.getElementById("pokemon-modal").classList.add("hidden");});
