c042714d926b0d12df02dc33c51dc159f9652f03
oscar-LCP <ofcruz@uniboyaca.edu.co>
2026-08-26 19:28:07 -0500

    Se creó el modelo de los pokemones

--- src/models/Pokemon.js
+++ src/models/Pokemon.js
@@ -1,8 +1,12 @@
 export default class Pokemon {
-    constructor(id, name, types, sprite) {
+    constructor(id, name, types, sprite, size, weight, abilities, stats) {
         this.id = id;
         this.name = name;
         this.types = types;
         this.sprite = sprite;
+        this.size = size;
+        this.weight = weight;
+        this.abilities = abilities;
+        this.stats = stats;
     }
 }
